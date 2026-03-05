62cc3f45db628af8f4a6875753213135a880a19b
Krit3d <kritsprog@gmail.com>
2026-03-05 18:22:20 +0500

    feat: add error handling for api request

--- API_Scout/main.py
+++ API_Scout/main.py
@@ -1,10 +1,21 @@
 import requests
 
 url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
-response = requests.get(url)
 
-# Превращаем JSON-ответ в словарь Python
-data = response.json()
+try:
+    response = requests.get(url)
+except requests.exceptions.RequestException as e:
+    # В случае исключения сообщаем об этом пользователю
+    print(f"Your request returned an exception: {type(e).__name__}.")
+    print("Please check your internet connection")
+else:
+    if response.status_code == 200:
+        # Превращаем JSON-ответ в словарь Python
+        data = response.json()
 
-# Выводим по ключу
-print(f"Текущая стоимость 1 Биткоина($): {data.get("bitcoin", {}).get("usd", "No data")}")
+        # Выводим по ключу
+        print(
+            f"The current value of 1 BTC($): {data.get("bitcoin", {}).get("usd", "No data")}"
+        )
+    else:
+        print("Server Error. Please wait or check if your request is right")
