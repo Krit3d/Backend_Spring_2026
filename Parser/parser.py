16c57e2ce50688f2c6f3463732940dd82eadaf7a
Krit3d <kritsprog@gmail.com>
2026-03-07 20:49:24 +0500

    feat: add csv export and news handling to parser

--- Parser/parser.py
+++ Parser/parser.py
@@ -1,15 +1,53 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
-url = "https://news.ycombinator.com/"
-r = requests.get(url)
+MAIN_URL = "https://news.ycombinator.com/"
 
-# Превращаем сырой HTML-текст в объект BeautifulSoup, с которым удобно работать
-soup = BeautifulSoup(r.text, "html.parser")
 
-# Выбираем все теги <a> по css-селектору.
-# Аналогично можно сделать при помощи .find_all()
-news_titles = soup.select(".titleline > a")
-# Выводим текст(заголовок) у каждого тега(новости)
-for t in news_titles:
-    print(t.text, end="\n")
+def get_news_page(page_num=1):
+    r = requests.get(MAIN_URL + "?p=" + str(page_num))
+
+    # Превращаем сырой HTML-текст в объект BeautifulSoup, с которым удобно работать
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    # Выбираем все нужные теги по css-селектору.
+    # Аналогично можно сделать при помощи .find_all()
+    news_titles = soup.select(".titleline > a")
+    news_scores = soup.select(".score")
+    news_ages = soup.select(".age")
+
+    # Объединяем данные каждой из новостей в отельные кортежи
+    news_total = zip(news_titles, news_scores, news_ages)
+
+    return news_total
+
+
+def fill_csv(news_items, csv_writer):
+    # Распаковываем каждый кортеж с данными
+    # Записываем эти данные в строки
+    for t, s, a in news_items:
+        title = t.text
+        link = t.get("href")
+        if link.startswith("item"):
+            link = MAIN_URL + link
+
+        points = s.text
+        age = a.text
+
+        csv_writer.writerow([title, link, points, age])
+
+
+# Создаём csv-файл
+with open(
+    "./Parser/hacker_news.csv", mode="w", encoding="utf-8", newline=""
+) as file:
+    writer = csv.writer(file)
+    writer.writerow(["Title", "Link", "Points", "Age"])  # Пишем шапку таблицы
+
+    # получаем новости постранично(пагинация)
+    news = [get_news_page(i + 1) for i in range(5)]
+
+    # Заполняем файл строками c данными о каждой новости
+    for n in news:
+        fill_csv(n, writer)
